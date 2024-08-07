b535dd00ad7e6cd795e3fe1dfdd4f8e8b9b19946
Simon Engelhardt <smn@nglhrdt.de>
2024-08-07 14:14:02 +0000

    chore:  fallback database config suitable to devcontainer config

--- src/config.ts
+++ src/config.ts
@@ -10,9 +10,9 @@ export const config = {
     },
     database: {
         host: process.env.POSTGRES_HOST || "localhost",
-        username: process.env.POSTGRES_USERNAME || "",
-        password: process.env.POSTGRES_PASSWORD || "",
         port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
-        database: process.env.POSTGRES_DATABASE || "inventory",
+        username: process.env.POSTGRES_USERNAME || "postgres",
+        password: process.env.POSTGRES_PASSWORD || "postgres",
+        database: process.env.POSTGRES_DATABASE || "postgres",
     }
 };
